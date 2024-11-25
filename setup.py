57722ee5390f5ddbd76d970827fd65a9b1327b27
Germán Ferrando <76514163+german36-del@users.noreply.github.com>
2024-11-25 14:59:05 +0100

    Update setup.py to include configs

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ import warnings
 from setuptools import find_packages, setup
 
 import torch
-from torch.utils.cpp_extension import (BuildExtension, CppExtension,
-                                       CUDAExtension)
+from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension
 
 
 def readme():
@@ -28,54 +27,18 @@ def get_version():
     return locals()['__version__']
 
 
-def make_cuda_ext(name, module, sources, sources_cuda=[]):
-
-    define_macros = []
-    extra_compile_args = {'cxx': []}
-
-    if torch.cuda.is_available() or os.getenv('FORCE_CUDA', '0') == '1':
-        define_macros += [('WITH_CUDA', None)]
-        extension = CUDAExtension
-        extra_compile_args['nvcc'] = [
-            '-D__CUDA_NO_HALF_OPERATORS__',
-            '-D__CUDA_NO_HALF_CONVERSIONS__',
-            '-D__CUDA_NO_HALF2_OPERATORS__',
-        ]
-        sources += sources_cuda
-    else:
-        print(f'Compiling {name} without CUDA')
-        extension = CppExtension
-
-    return extension(
-        name=f'{module}.{name}',
-        sources=[os.path.join(*module.split('.'), p) for p in sources],
-        define_macros=define_macros,
-        extra_compile_args=extra_compile_args)
-
-
 def parse_requirements(fname='requirements.txt', with_version=True):
     """Parse the package dependencies listed in a requirements file but strips
-    specific versioning information.
-
-    Args:
-        fname (str): path to requirements file
-        with_version (bool, default=False): if True include version specs
-
-    Returns:
-        List[str]: list of requirements items
-
-    CommandLine:
-        python -c "import setup; print(setup.parse_requirements())"
-    """
+    specific versioning information."""
     import re
     import sys
     from os.path import exists
+
     require_fpath = fname
 
     def parse_line(line):
         """Parse information from a line in a requirements text file."""
         if line.startswith('-r '):
-            # Allow specifying requirements in other files
             target = line.split(' ')[1]
             for info in parse_require_file(target):
                 yield info
@@ -86,7 +49,6 @@ def parse_requirements(fname='requirements.txt', with_version=True):
             elif '@git+' in line:
                 info['package'] = line
             else:
-                # Remove versioning from the package
                 pat = '(' + '|'.join(['>=', '==', '>']) + ')'
                 parts = re.split(pat, line, maxsplit=1)
                 parts = [p.strip() for p in parts]
@@ -95,13 +57,10 @@ def parse_requirements(fname='requirements.txt', with_version=True):
                 if len(parts) > 1:
                     op, rest = parts[1:]
                     if ';' in rest:
-                        # Handle platform specific dependencies
-                        # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-platform-specific-dependencies
-                        version, platform_deps = map(str.strip,
-                                                     rest.split(';'))
+                        version, platform_deps = map(str.strip, rest.split(';'))
                         info['platform_deps'] = platform_deps
                     else:
-                        version = rest  # NOQA
+                        version = rest
                     info['version'] = (op, version)
             yield info
 
@@ -120,7 +79,6 @@ def parse_requirements(fname='requirements.txt', with_version=True):
                 if with_version and 'version' in info:
                     parts.extend(info['version'])
                 if not sys.version.startswith('3.4'):
-                    # apparently package_deps are broken in 3.4
                     platform_deps = info.get('platform_deps')
                     if platform_deps is not None:
                         parts.append(';' + platform_deps)
@@ -131,62 +89,7 @@ def parse_requirements(fname='requirements.txt', with_version=True):
     return packages
 
 
-def add_mim_extension():
-    """Add extra files that are required to support MIM into the package.
-
-    These files will be added by creating a symlink to the originals if the
-    package is installed in `editable` mode (e.g. pip install -e .), or by
-    copying from the originals otherwise.
-    """
-
-    # parse installment mode
-    if 'develop' in sys.argv:
-        # installed by `pip install -e .`
-        if platform.system() == 'Windows':
-            # set `copy` mode here since symlink fails on Windows.
-            mode = 'copy'
-        else:
-            mode = 'symlink'
-    elif 'sdist' in sys.argv or 'bdist_wheel' in sys.argv:
-        # installed by `pip install .`
-        # or create source distribution by `python setup.py sdist`
-        mode = 'copy'
-    else:
-        return
-
-    filenames = [
-        'tools', 'configs', 'demo', 'model-index.yml', 'dataset-index.yml'
-    ]
-    repo_path = osp.dirname(__file__)
-    mim_path = osp.join(repo_path, 'mmdet', '.mim')
-    os.makedirs(mim_path, exist_ok=True)
-
-    for filename in filenames:
-        if osp.exists(filename):
-            src_path = osp.join(repo_path, filename)
-            tar_path = osp.join(mim_path, filename)
-
-            if osp.isfile(tar_path) or osp.islink(tar_path):
-                os.remove(tar_path)
-            elif osp.isdir(tar_path):
-                shutil.rmtree(tar_path)
-
-            if mode == 'symlink':
-                src_relpath = osp.relpath(src_path, osp.dirname(tar_path))
-                os.symlink(src_relpath, tar_path)
-            elif mode == 'copy':
-                if osp.isfile(src_path):
-                    shutil.copyfile(src_path, tar_path)
-                elif osp.isdir(src_path):
-                    shutil.copytree(src_path, tar_path)
-                else:
-                    warnings.warn(f'Cannot copy file {src_path}.')
-            else:
-                raise ValueError(f'Invalid mode {mode}')
-
-
 if __name__ == '__main__':
-    add_mim_extension()
     setup(
         name='mmdet',
         version=get_version(),
@@ -197,8 +100,11 @@ if __name__ == '__main__':
         author_email='openmmlab@gmail.com',
         keywords='computer vision, object detection',
         url='https://github.com/open-mmlab/mmdetection',
-        packages=find_packages(exclude=('configs', 'tools', 'demo')),
+        packages=find_packages(exclude=('tools', 'demo')),
         include_package_data=True,
+        package_data={
+            'mmdet': ['configs/**/*', 'model-index.yml', 'dataset-index.yml']
+        },
         classifiers=[
             'Development Status :: 5 - Production/Stable',
             'License :: OSI Approved :: Apache Software License',
@@ -221,4 +127,5 @@ if __name__ == '__main__':
         },
         ext_modules=[],
         cmdclass={'build_ext': BuildExtension},
-        zip_safe=False)
+        zip_safe=False
+    )
